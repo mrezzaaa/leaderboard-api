ba1a5cd0509efad4e4c6ff3fc760759945211bfe
mrezzaaa <mrezzaaa@gmail.com>
2024-06-13 05:10:32 +0700

    Fixed error logging

--- src/score/score.module.ts
+++ src/score/score.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScoreService } from './score.service';
 import { ScoreController } from './score.controller';
 import { Score } from './score.entity';
 import { Users } from '../users/users.entity';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { LoggingMiddleware } from './logging/logging.middleware';
+import { LoggingMiddleware } from '../logging/logging.middleware';
 @Module({
     imports: [TypeOrmModule.forFeature([Score,Users])],
     exports: [TypeOrmModule]
